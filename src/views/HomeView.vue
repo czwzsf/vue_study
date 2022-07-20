b5e69efdd94eb0a5ed82f228b4e799a93463b8ae
zhewei chen <1241770441@qq.com>
2022-07-20 15:11:54 +0800

    Vue指令学习

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
+    <HelloWorld/>
+    <HelloWorld/>
+<!--    <my-component-name/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+// import Vue from "vue"
 import HelloWorld from '@/components/HelloWorld.vue'
-
+//
+// Vue.component('my-component-name', {
+//   template: <h3>我的第一个局部组件</h3>
+// })
 export default {
   name: 'HomeView',
   components: {
